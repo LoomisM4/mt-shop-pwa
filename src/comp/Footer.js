e9714224e0cefd13dc8906a2c9b9fcd6026d23a2
LoomisM4 <mail@marcel-wettach.com>
2022-04-09 10:24:29 +0200

    changed to relative paths

--- src/comp/Footer.js
+++ src/comp/Footer.js
@@ -6,22 +6,22 @@ export default function Footer() {
         <footer>
             <div className={"navLink"} onClick={() => window.location = "/"}>
                 <div>
-                    <img src="img/star.png"/>
+                    <img src="/img/star.png"/>
                 </div>
             </div>
             <div className={"navLink"} onClick={() => window.location = "/categories"}>
                 <div>
-                    <img src="img/list.png"/>
+                    <img src="/img/list.png"/>
                 </div>
             </div>
             <div className={"navLink"} onClick={() => window.location = "/cart"}>
                 <div>
-                    <img src="img/cart.png"/>
+                    <img src="/img/cart.png"/>
                 </div>
             </div>
             <div className={"navLink"} onClick={() => window.location = "/map"}>
                 <div>
-                    <img src="img/map.png"/>
+                    <img src="/img/map.png"/>
                 </div>
             </div>
         </footer>

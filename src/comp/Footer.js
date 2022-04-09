49cfef41f7c0cb160bcf6f3b335f34ccef135517
LoomisM4 <mail@marcel-wettach.com>
2022-04-09 10:21:13 +0200

    placed icons in the footer

--- src/comp/Footer.js
+++ src/comp/Footer.js
@@ -4,10 +4,26 @@ import '../style/Footer.css';
 export default function Footer() {
     return(
         <footer>
-            <div onClick={() => window.location = "/"}>Spotlight</div>
-            <div onClick={() => window.location = "/categories"}>Categories</div>
-            <div onClick={() => window.location = "/cart"}>Cart</div>
-            <div onClick={() => window.location = "/map"}>Map</div>
+            <div className={"navLink"} onClick={() => window.location = "/"}>
+                <div>
+                    <img src="img/star.png"/>
+                </div>
+            </div>
+            <div className={"navLink"} onClick={() => window.location = "/categories"}>
+                <div>
+                    <img src="img/list.png"/>
+                </div>
+            </div>
+            <div className={"navLink"} onClick={() => window.location = "/cart"}>
+                <div>
+                    <img src="img/cart.png"/>
+                </div>
+            </div>
+            <div className={"navLink"} onClick={() => window.location = "/map"}>
+                <div>
+                    <img src="img/map.png"/>
+                </div>
+            </div>
         </footer>
     )
 }
